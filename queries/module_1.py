903254ca03163fbf7c60e637166a785dc6fe7f7e
mbgoodguy <nerooren4001@gmail.com>
2024-03-20 15:47:58 +0300

    Use AsyncSession instead async_sessionmaker

--- queries/module_1.py
+++ queries/module_1.py
@@ -1,14 +1,12 @@
 import asyncio
 
 from sqlalchemy import String
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, create_session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 sqlite_url = "sqlite+aiosqlite:///basic_db.sqlite3"
-engine = create_async_engine(url=sqlite_url, connect_args={"check_same_thread": False})
-
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
-async_session = SessionLocal()
+engine = create_async_engine(url=sqlite_url, connect_args={"check_same_thread": False}, future=True)
+async_session = AsyncSession(bind=engine)
 
 
 class Base(DeclarativeBase):
@@ -30,15 +28,17 @@ async def init_models():
         print("Таблицы созданы")
 
 
-async def create_user(name: str, username: str):
-    async with async_session as session:  # Используйте вызов асинхронного сеанса
-        user = User(name=name, username=username)
+async def create_user(username: str):
+    async with async_session as session:
+        user = User(username=username)
         session.add(user)
-        await session.commit()  # Асинхронный вызов метода commit()
-        print(f"User has been created. User's id: {user.id}")
-        print(user.id)
+        await session.commit()
+
+
+async def main():
+    await init_models()
+    await create_user(username='Vina Sky')
 
 
 if __name__ == '__main__':
-    asyncio.run(init_models())
-    asyncio.run(create_user(name='Megan', username='Fox'))
+    asyncio.run(main())
